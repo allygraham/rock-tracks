6d2d6c2233d56e5cf97734ddbf54a0cae5b3fd80
Ally Graham <agraham@scottishpower.com>
2017-08-25 16:43:30 +0100

    Updated track page to be better responsive.

--- src/TrackPage.js
+++ src/TrackPage.js
@@ -71,13 +71,13 @@ class TrackPage extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <div className="col-xs-12 col-md-8">
+                    <div className="col-xs-12 col-md-6 col-lg-8">
                         {this.getTrackView()}
-                        <div className="col-xs-12 col-md-6" style={{ padding: 0 }}>
+                        <div className="col-xs-12 col-md-8 col-lg-6" style={{ padding: 0 }}>
                             {this.getBackButton()}
                         </div>
                     </div>
-                    <div className="col-xs-12 col-md-4">
+                    <div className="col-xs-12 col-md-6 col-lg-4">
                         <RandomTracks content={this.props.tracks} />
                     </div>
                 </div>
